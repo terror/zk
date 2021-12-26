707308853d892fbafb45f675a7c043b574402399
Liam <liam@scalzulli.com>
2021-12-25 23:28:51 -0500

    Cleaner handler.rs

--- src/handler.rs
+++ src/handler.rs
@@ -15,29 +15,25 @@ impl Handler {
   /// with an appropriate prefix, in addition to writing the default YAML
   /// frontmatter.
   pub fn create(&self, name: &str) -> Result<()> {
-    Note::create(self.directory.path.join(NoteId::new(name).to_string()))?;
-    self.open(name)?;
-    Ok(())
+    self.open(
+      &Note::create(self.directory.path.join(NoteId::new(name).to_string()))?
+        .id
+        .name,
+    )
   }
 
   /// Opens a note given a `name` using the editor specified in the
   /// configuration file. If there are multiple notes present with the
   /// same `name`, the user will be prompted with `skim` to choose
   /// which file is desired to be opened.
-  pub(crate) fn open(&self, name: &str) -> Result<(), Error> {
-    let candidates = self.directory.find(name)?;
-
-    if candidates.len() == 1 {
-      let note = candidates.first().unwrap();
-      Command::new(&self.config.editor).arg(&note.path).status()?;
-      return Ok(());
-    }
-
-    for note in Search::new(candidates).run()? {
-      Command::new(&self.config.editor).arg(&note.path).status()?;
-    }
-
-    Ok(())
+  pub(crate) fn open(&self, name: &str) -> Result<()> {
+    Search::new(self.directory.find(name)?)
+      .run()?
+      .iter()
+      .try_for_each(|note| {
+        Command::new(&self.config.editor).arg(&note.path).status()?;
+        Ok(())
+      })
   }
 
   /// Links two notes together. This entails checking and modifying both notes'
@@ -64,27 +60,30 @@ impl Handler {
   /// Finds all notes given a `tag`. This method invokes `skim` using the
   /// names of the notes that contain `tag` within the frontmatter and
   /// attempts to open each selected item.
-  pub(crate) fn find(&self, tag: &str) -> Result<(), Error> {
-    let candidates = self.directory.find_by_tag(tag)?;
-
-    for note in Search::new(candidates).run()? {
-      Command::new(&self.config.editor).arg(&note.path).status()?;
-    }
-
-    Ok(())
+  pub(crate) fn find(&self, tag: &str) -> Result<()> {
+    Search::new(self.directory.find_by_tag(tag)?)
+      .run()?
+      .iter()
+      .try_for_each(|note| {
+        Command::new(&self.config.editor).arg(&note.path).status()?;
+        Ok(())
+      })
   }
 
   /// Starts a fuzzy search using note id's in the Zettelkasten directory.
   /// Powered by `skim` --> https://github.com/lotabout/skim
-  pub(crate) fn search(&self) -> Result<(), Error> {
-    for note in Search::new(self.directory.notes()?).run()? {
-      Command::new(&self.config.editor).arg(&note.path).status()?;
-    }
-    Ok(())
+  pub(crate) fn search(&self) -> Result<()> {
+    Search::new(self.directory.notes()?)
+      .run()?
+      .iter()
+      .try_for_each(|note| {
+        Command::new(&self.config.editor).arg(&note.path).status()?;
+        Ok(())
+      })
   }
 
   /// Writes the current Zettelkasten storage location to stdout.
-  pub fn dir(&self) -> Result<(), Error> {
+  pub fn dir(&self) -> Result<()> {
     println!("{}", self.directory.path.expand().display());
     Ok(())
   }
@@ -93,40 +92,22 @@ impl Handler {
   /// also prompt the user if more than one note exists with `name`.
   /// This method should scan all notes with a link to the soon to be
   /// deleted note and remove those links.
-  pub(crate) fn remove(&self, name: &str) -> Result<(), Error> {
-    let candidates = self.directory.find(name)?;
-
-    if candidates.len() == 1 {
-      let candidate = candidates.first().unwrap();
-
-      let id = &candidate.id.to_string();
-
-      for note in self.directory.notes()? {
-        let mut note = note;
-        if note.has_link(id) {
-          note.remove_link(id)?;
-        }
-      }
-
-      candidate.remove()?;
-
-      return Ok(());
-    }
-
-    for candidate in Search::new(candidates).run()? {
-      let id = &candidate.id.to_string();
-
-      for note in self.directory.notes()? {
-        let mut note = note;
-        if note.has_link(id) {
-          note.remove_link(id)?;
-        }
-      }
-
-      candidate.remove()?;
-    }
-
-    Ok(())
+  pub(crate) fn remove(&self, name: &str) -> Result<()> {
+    Search::new(self.directory.find(name)?)
+      .run()?
+      .iter()
+      .try_for_each(|item| {
+        self
+          .directory
+          .notes()?
+          .iter_mut()
+          .filter(|note| note.has_link(&item.id.to_string()))
+          .try_for_each(|note| -> Result<()> {
+            note.remove_link(&item.id.to_string())?;
+            Ok(())
+          })?;
+        item.remove()
+      })
   }
 
   /// Removes a link between two existing notes.
@@ -151,42 +132,26 @@ impl Handler {
 
   /// Adds a tag to an existing note.
   pub(crate) fn tag(&self, name: &str, tag: &str) -> Result<()> {
-    let candidates = self.directory.find(name)?;
-
-    if candidates.len() == 1 {
-      let mut candidate = candidates.first().unwrap().to_owned();
-      candidate.add_tag(tag)?;
-      return Ok(());
-    }
-
-    for note in Search::new(candidates).run()? {
-      let mut note = note;
-      if !note.has_tag(tag) {
+    Search::new(self.directory.find(name)?)
+      .run()?
+      .iter_mut()
+      .filter(|note| !note.has_tag(tag))
+      .try_for_each(|note| {
         note.add_tag(tag)?;
-      }
-    }
-
-    Ok(())
+        Ok(())
+      })
   }
 
   /// Removes a tag from an existing note.
   pub(crate) fn remove_tag(&self, name: &str, tag: &str) -> Result<()> {
-    let candidates = self.directory.find(name)?;
-
-    if candidates.len() == 1 {
-      let mut candidate = candidates.first().unwrap().to_owned();
-      candidate.remove_tag(tag)?;
-      return Ok(());
-    }
-
-    for note in Search::new(candidates).run()? {
-      let mut note = note;
-      if note.has_tag(tag) {
+    Search::new(self.directory.find(name)?)
+      .run()?
+      .iter_mut()
+      .filter(|note| note.has_tag(tag))
+      .try_for_each(|note| {
         note.remove_tag(tag)?;
-      }
-    }
-
-    Ok(())
+        Ok(())
+      })
   }
 
   /// Explore a notes links recursively.
